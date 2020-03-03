7c012a231b3199fe07cbb9a4f1240c9e4b82806c
Lareta <lareta064@gmail.com>
2020-03-03 22:42:33 +0300

    new page

---
+++ src/S_Dialog_media/S_Dialog_player_embed.js
@@ -0,0 +1,2 @@
+var oeTags = '<img src="S_Dialog_media/S_Dialog.gif" width="950" height="700" alt=""/>';         
+document.write( oeTags );
--- src/js/main.js
+++ src/js/main.js
@@ -3,31 +3,122 @@ $(document).ready(function() {
 		$('#number-slider').slick({
 			dots: true,
       		slidesToShow: 5,
-	slidesToScroll: 1,
-	responsive: [
+			slidesToScroll: 1,
+			speed: 950,
+			infinite: true,
+			autoplay: true,
+			responsive: [
+				{
+			      breakpoint: 1200,
+			      settings: {
+			        slidesToShow: 4,
+			      }
+			    },
+			    {
+			      breakpoint: 992,
+			      settings: {
+			        slidesToShow: 3,
+			      }
+			    },
+			    {
+			      breakpoint: 760,
+			      settings: {
+			        slidesToShow: 2,
+
+			      }
+			    },
+			    {
+			      breakpoint: 425,
+			      settings: {
+			        slidesToShow: 1,
+
+			      }
+			    }
+		    ]
+			});
+
+ })
+
+
+	$(function(){
+		$('#teamSliderMobile').slick({
+			dots: true,
+	      	slidesToShow: 1,
+			slidesToScroll: 1,
+			variableWidth: true,
+			speed: 450,
+		});
+
+	})
+	$('#teamSlider').slick({
+		slidesToShow: 4,
+		infinite: true,
+		arrows:true,
+		speed: 950,
+		slidesToScroll: 1,
+
+		nextArrow: '<div class="teamSlider--arrow"><i class="fas fa-caret-right"></i></div>',
+		prevArrow: '<div class="teamSlider--arrow"><i class="fas fa-caret-left"></i></i></div>',
+		responsive: [
 		{
 	      breakpoint: 1200,
 	      settings: {
 	        slidesToShow: 4,
-	      }
-	    },
-	    {
+	      },
 	      breakpoint: 992,
 	      settings: {
 	        slidesToShow: 3,
 	      }
-	    },
-	    {
-	      breakpoint: 480,
-	      settings: {
-	        slidesToShow: 2,
-
-	      }
-	    }
-    ]
+	    }]
 	});
- })
-})
+
+	$('#reviewsSlider').slick({
+		dots: false,
+		vertical: true,
+		verticalSwiping: true,
+		slidesToShow: 2,
+		nextArrow: '<i class="fas fa-sort-down"></i>',
+		prevArrow: '<i class="fas fa-sort-up"></i>',
+		senteredMode: true,
+		responsive: [
+		{
+	      breakpoint: 992,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }]
+	});
+
+	// отправка почты
+		var userPhone = $(".phone").val();
+		console.log(userPhone)
+      $("#send-form").on('click', function(event){
+        event.preventDefault();
+        var userPhone = $(".phone").val();
+        var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+        // Формируем ajax запрос
+        $.ajax({
+          type: "POST", // Тип запроса - POST
+          url: "php/mail.php", // Куда отправляем запрос
+          data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+          // Функция если все прошло успешно
+          success: function(html){
+            $("#contact-form").slideUp(800);
+            $('#answer').html(html);
+          }
+        });
+
+        // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+        return false;
+
+
+      });
+
+      $(".phone").mask("+7(999)999-99-99");
+
+});
 //-клик по гамбургеру
 var menuToggle = document.querySelector('.cmn-toggle-switch');
 var menuMobile = document.querySelector('#header-nav');
